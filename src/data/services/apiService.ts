ea87a469c8d2e72b01cfcaf3357307b2e84ec13a
netanel amrami <amraminetanel@gmail.com>
2025-08-01 14:08:21 +0300

    api

--- src/data/services/apiService.ts
+++ src/data/services/apiService.ts
@@ -1,14 +1,14 @@
 const BASE_URL = "https://api.pixshare.live/PixApi/api";
 
 export const apiService = {
-  async getEvent(eventLink: string = '2d115c1c-guy-sigal') {
+  async getEvent(eventLink: string) {
     const res = await fetch(`${BASE_URL}/Event/getByEventLink?eventLink=${eventLink}`);
     if (!res.ok) throw new Error("Failed fetching event data");
     const eventData = await res.json();
     return eventData;
   },
 
-  async getEventImagesFullData(eventLink: string = '2d115c1c-guy-sigal') {
+  async getEventImagesFullData(eventLink: string) {
     // קודם נקבל את ה-event כדי לחלץ את ה-eventId
     const eventData = await this.getEvent(eventLink);
     const eventId = eventData.id;
